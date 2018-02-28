a98efbef5ea52274749e570d4c1f4cc29bfe2166
Astro <astro@spaceboyz.net>
2018-02-28 01:07:59 +0100

    examples/ip: use _heap_size instead of _eheap

--- examples/ip.rs
+++ examples/ip.rs
@@ -64,15 +64,15 @@ pub static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
 // These symbols come from a linker script
 extern "C" {
     static mut _sheap: u32;
-    static mut _eheap: u32;
+    static mut _heap_size: u32;
 }
 
 /// Initialize the heap allocator `ALLOCATOR`
 pub fn init_alloc() -> usize {
     let start = unsafe { &mut _sheap as *mut u32 as usize };
-    let end = unsafe { &mut _eheap as *mut u32 as usize };
-    unsafe { ALLOCATOR.init(start, end - start) }
-    end - start
+    let size = unsafe { &mut _heap_size as *mut u32 as usize };
+    unsafe { ALLOCATOR.init(start, size); }
+    size
 }
 
 fn main() {
